a0f611d97420fc702d9811f370ad265b701ea5ca
Ansu Singh <ansumsingh@gmail.com>
2024-12-16 16:46:48 +1100

    Sync to latest assignments 3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 51fd3039027b0bea43e466c5431aaef774dcd9e3
+AESD_ASSIGNMENTS_VERSION = 7c8189216fafcfb20e84b70f2569076590ecbe7b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
